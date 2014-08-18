e950cc35a75cd1b1371e77f5ec42dd7be4f79c1f
Jesper Martinussen <jesperkmartinussen@gmail.com>
2014-08-18 23:01:08 +0200

    adding new version

--- header.php
+++ header.php
@@ -6,24 +6,22 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<title>Jesper Portfolio</title>
 	<link rel="stylesheet" href="css/style.css" type="text/css" />
-	<link href='http://fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-</head>
+	<link href='http://fonts.googleapis.com/css?family=Lato:700' rel='stylesheet' type='text/css'>
 
 <body>
 <div id="wrapper">
 
 <header>
-	<div id="nav-wrap">
-		<div id="logo"></div>	
-		<nav class="clearfix">
-			<ul class="clearfix">
-				<li><a data-scroll href="#home-banner" class="active">Home</a></li>
-				<li><a data-scroll href="#my-work">Work</a></li>
-				<li><a data-scroll href="#skills">Skills</a></li>
-				<li><a data-scroll href="#about">About</a></li>
-				<li><a data-scroll href="#contact">Contact</a></li>
-			</ul>
-			<a href="#" id="pull" class="clearfix"><p>Menu</p></a> 
-		</nav>
-	</div>
+	<nav>
+		<ul class="clearfix">
+			<li><a data-scroll href="#my-work" class="active">Work</a></li>
+			<li><a data-scroll href="#skills">Skills</a></li>
+		</ul>
+		<a data-scroll href="#home-banner"><img src="img/jm-logo.png" alt="logo" id="logo" /></a>
+		<ul>	
+			<li><a data-scroll href="#about">About</a></li>
+			<li><a data-scroll href="#contact">Contact</a></li>
+		</ul>
+		<a href="#" id="pull" class="clearfix"><p>Menu</p></a> 
+	</nav>
 </header>
--- index.php
+++ index.php
@@ -9,12 +9,28 @@
 </section>
 
 <section id="my-work">
+	<h1>My Work</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt accusamus, nisi voluptate perspiciatis sapiente repellendus cum, deleniti reprehenderit quae quia atque unde et, laboriosam error quas quod minima numquam deserunt.</p>
+		
+		<div id="work" class="suncreature">
+			<h1>Sun Creature Studio<h1>
+		</div>
+
+		<div id="work" class="brammingtand">
+			<h1>Bramming Dentist</h1>
+		</div>
+
+		<div id="work" class="alethrion">
+			<h1>The Tales of Alethrion</h1>
+		</div>
 </section>
 
 <section id="skills">
+	<h1>My Skills</h1>
 </section>
 
 <section id="about">
+	<h1>About</h1>
 </section>
 
 <section id="contact">
@@ -28,3 +44,4 @@
 
 
 <?php include "footer.php"; ?>
+
